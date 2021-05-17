b70db9ac210e4d7f54d6a49025aa786add82764f
Bansi-Iqhired <bansi@iqhired.com>
2021-05-17 18:00:11 +0530

    Update footer.php

--- wp-content/themes/astra/footer.php
+++ wp-content/themes/astra/footer.php
@@ -28,9 +28,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 	astra_footer_after(); 
 ?>
 	</div><!-- #page -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php 
+
+  
+    $upload_dir = site_url() . '/wp-content/uploads/pdf';
+
+?>
+
+	<input type="hidden" id="ajax_url" value="<?php echo admin_url('admin-ajax.php');?>">
+	<input type ="hidden" id="current_id" value="<?php echo $post->ID; ?>">
+	<input type="hidden" id="upload_url" value="<?php echo $upload_dir;?>">
+	<script>
+	//jQuery(document).ready( function(){         
+ $(window).on('load', function(){
+       
+//var rml_post_id = jQuery(this).data( 'id' );  
+//var ajax_url = $("#ajax_url").val();	
+var post_id = $("#current_id").val();	
+var upload_dir = $("#upload_url").val();
+var newUrl =  upload_dir + "/" + post_id + ".pdf" ;
+console.log(post_id);
+				            $(".download_pdf ").attr("href", newUrl); // Set herf value
+          
+    });     
+//});
+	</script>
 <?php 
 	astra_body_bottom();    
 	wp_footer(); 
+	
 ?>
 	</body>
 </html>
